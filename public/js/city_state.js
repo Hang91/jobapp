01ff32362ec8c9a3e56499fee9c4df6e84d80cfe
Ying Zhu <yingzhu@Yings-MacBook-Pro.local>
2017-06-29 00:13:01 -0400

    show all at the beginning when search by locationand can search all

--- public/js/city_state.js
+++ public/js/city_state.js
@@ -269,7 +269,7 @@ city_states['Venezuela'] = '|Caracas||Amazonas|Anzoategui|Apure|Aragua|Barinas|B
 function setRegions()
 {
 	for (region in countries)
-		document.write('<option value="' + region + '">' + region + '</option>');
+		document.write('<option value="' + region + '">' + region + '</option>');	
 }
 
 function set_country(oRegionSel, oCountrySel, oCity_StateSel)
@@ -281,15 +281,21 @@ function set_country(oRegionSel, oCountrySel, oCity_StateSel)
 	if (countries[region])
 	{
 		oCountrySel.disabled = false;
-		oCity_StateSel.disabled = true;
-		oCountrySel.options[0] = new Option('SELECT COUNTRY','');
+		oCountrySel.options[0] = new Option('ALL','');
 		countryArr = countries[region].split('|');
 		for (var i = 0; i < countryArr.length; i++)
 			oCountrySel.options[i + 1] = new Option(countryArr[i], countryArr[i]);
 		document.getElementById('txtregion').innerHTML = region;
 		document.getElementById('txtplacename').innerHTML = '';
+		oCity_StateSel.disabled = true;
+		oCity_StateSel.options[0] = new Option('ALL','');
 	}
-	else oCountrySel.disabled = true;
+	else{
+		oCountrySel.disabled = true;
+		oCountrySel.options[0] = new Option('ALL','');
+		oCity_StateSel.disabled = true;
+		oCity_StateSel.options[0] = new Option('ALL','');
+	}
 }
 
 function set_city_state(oCountrySel, oCity_StateSel)
@@ -300,13 +306,16 @@ function set_city_state(oCountrySel, oCity_StateSel)
 	if (city_states[country])
 	{
 		oCity_StateSel.disabled = false;
-		oCity_StateSel.options[0] = new Option('SELECT NEAREST DIVISION','');
+		oCity_StateSel.options[0] = new Option('ALL','');
 		city_stateArr = city_states[country].split('|');
 		for (var i = 0; i < city_stateArr.length; i++)
 			oCity_StateSel.options[i+1] = new Option(city_stateArr[i],city_stateArr[i]);
 		document.getElementById('txtplacename').innerHTML = country;
 	}
-	else oCity_StateSel.disabled = true;
+	else{
+		oCity_StateSel.disabled = true;
+		oCity_StateSel.options[0] = new Option('ALL','');
+	}
 }
 
 function print_city_state(oCountrySel, oCity_StateSel)
